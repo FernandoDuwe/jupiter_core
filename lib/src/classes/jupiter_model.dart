a38863a28fdfca657e7db71306efff91ff564cb5
Duwe <fernando_duwe@hotmail.com>
2024-09-30 12:47:20 -0300

    Creating custom request

--- lib/src/classes/jupiter_model.dart
+++ lib/src/classes/jupiter_model.dart
@@ -1,3 +1,19 @@
+import 'package:jupiter_core/jupiter_core.dart';
 import 'package:jupiter_core/src/classes/jupiter_object.dart';
 
-class JupiterModel extends JupiterObject {}
+class JupiterModel extends JupiterObject {
+  JupiterJson? fields;
+
+  JupiterModel();
+
+  JupiterModel.asFieldList(JupiterJson json) {
+    this.fields = json;
+  }
+
+  JupiterJson toJson() => this.fields ?? {};
+
+  bool fieldExists(String prFieldName) =>
+      this.toJson().containsKey(prFieldName);
+
+  dynamic fieldByName(String prFieldName) => this.toJson()[prFieldName];
+}
--- lib/src/classes/jupiter_repository.dart
+++ lib/src/classes/jupiter_repository.dart
@@ -1,3 +1,37 @@
-import 'package:jupiter_core/src/classes/jupiter_object.dart';
+import 'dart:convert';
 
-class JupiterRepository extends JupiterObject {}
+import 'package:dio/dio.dart';
+import 'package:jupiter_core/src/classes/jupiter_model.dart';
+import 'package:jupiter_core/src/classes/jupiter_object.dart';
+import 'package:jupiter_core/src/utils/types.dart';
+
+class JupiterRepository extends JupiterObject {
+  static Future<JupiterModel?> get(
+      String prEntryPoint, JupiterJson? prQueryParams) async {
+    final dio = Dio();
+
+    Response response =
+        await dio.get(prEntryPoint, queryParameters: prQueryParams);
+
+    dynamic vrReturnData = jsonDecode(response.data.toString());
+
+    return JupiterModel.asFieldList(vrReturnData as JupiterJson);
+  }
+
+  static Future<List<JupiterModel>> getMany(
+      String prEntryPoint, JupiterJson? prQueryParams) async {
+    final dio = Dio();
+
+    Response response =
+        await dio.get(prEntryPoint, queryParameters: prQueryParams);
+
+    dynamic vrReturnData = jsonDecode(response.data.toString());
+
+    List<JupiterModel> vrList = [];
+
+    (vrReturnData as List<JupiterJson>)
+        .forEach((element) => vrList.add(JupiterModel.asFieldList(element)));
+
+    return vrList;
+  }
+}
