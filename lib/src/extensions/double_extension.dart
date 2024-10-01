44bd2c6df4a635c0381c9953ed266c4208bae2d5
Duwe <fernando_duwe@hotmail.com>
2024-10-01 12:55:26 -0300

    percentOf function

--- lib/src/extensions/double_extension.dart
+++ lib/src/extensions/double_extension.dart
@@ -3,4 +3,6 @@ import 'package:intl/intl.dart';
 extension DoubleUtils on double {
   String toMoneyMask({String prLocation = "pt_BR"}) =>
       NumberFormat.simpleCurrency(locale: prLocation).format(this);
+
+  double percentOf(double prPercent) => (prPercent / 100).round() * this;
 }
